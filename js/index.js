8045e1a9b6c858b11c0cc10541bbfdc4d4527d43
sloth-hub <madamhh@gmail.com>
2022-04-30 21:14:49 +0900

    Add scroll animations

--- js/index.js
+++ js/index.js
@@ -4,30 +4,51 @@ const gallary = document.querySelector("div.gallary");
 const closeBtn = document.querySelector("span.close");
 const modal = document.querySelector("div.modal");
 const modalImg = modal.querySelector("img.gallary-img");
+const topbtn = document.querySelector("a.top");
 
 init();
 
 function init() {
-    clickedImage();
     window.addEventListener("scroll", scrollHeader);
+    AOS.init();
+    skillsScrollAnimate();
+    clickedImage();
 }
 
 function scrollHeader() {
     if (pageYOffset >= 70) {
         header.classList.add("on");
+        topbtn.classList.add("on");
     } else {
         header.classList.remove("on");
+        topbtn.classList.remove("on");
     }
+}
+
+function skillsScrollAnimate() {
+    window.addEventListener('scroll', function () {
+        const skills = document.querySelectorAll("circle.skillbar");
+        if (window.scrollY >= 1500 && window.scrollY <= 2800) {
+            skills.forEach((e) => {
+                e.classList.add("circle_animation");
+            });
+        } else {
+            skills.forEach((e) => {
+                e.classList.remove("circle_animation");
+            });
+        };
+    });
 }
 
 function clickedImage() {
 
     gallary.addEventListener("click", ({ target }) => {
-        if (target.className == "gallary-item") {
+        if (target.classList.contains("gallary-item")) {
             $(modal).css({ "display": "flex", "align-items": "flex-start" })
-                .hide()
-                .fadeIn();
+            .hide()
+            .fadeIn();
             header.classList.remove("on");
+            topbtn.classList.remove("on");
             body.style.overflow = "hidden";
             modalImg.src = target.children[0].src;
             modalImg.alt = target.children[0].alt;
@@ -52,6 +73,7 @@ function closedImage() {
         modalImg.classList.remove("zoom");
         body.style.overflow = "overlay";
         header.classList.add("on");
+        topbtn.classList.add("on");
         $(modal).fadeOut();
     });
 
