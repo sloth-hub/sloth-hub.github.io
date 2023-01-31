b6ac7ac37141526425e3e70fc1352b98494cf50e
sloth-hub <madamhh@gmail.com>
2023-01-31 21:53:58 +0900

    Chore: 코드 단순화

--- js/index.js
+++ js/index.js
@@ -1,25 +1,39 @@
 const body = document.querySelector("body");
 const header = document.querySelector("header");
-const gallary = document.querySelector("div.gallary");
+const work = document.querySelector("#works");
 const closeBtn = document.querySelector("span.close");
 const modal = document.querySelector("div.modal");
 const modalImg = modal.querySelector("img.gallary-img");
 const topbtn = document.querySelector("a.top");
-const toggleBar = document.querySelector("div.toggle-bar");
 const navmenu = document.querySelector("ul.nav-menu");
 
 init();
 
 function init() {
-    window.addEventListener("scroll", scrollHeader);
     AOS.init();
-    skillsScrollAnimate();
-    clickedToggleBar();
-    clickedImage();
+    window.addEventListener("scroll", scrollEvent);
+    body.addEventListener("click", ({ target }) => {
+        if (target.className === "toggle-bar") {
+            clickedToggleBar();
+        } else if (target.closest("section").id === "works") {
+            clickedImage(target);
+        }
+    });
 }
 
-function scrollHeader() {
-    if (pageYOffset >= 70) {
+function scrollEvent() {
+    const skills = document.querySelectorAll("circle.skillbar");
+    if (window.scrollY >= 1500 && window.scrollY <= 2650) {
+        skills.forEach((e) => {
+            e.classList.add("circle_animation");
+        });
+    } else {
+        skills.forEach((e) => {
+            e.classList.remove("circle_animation");
+        });
+    };
+
+    if (window.pageYOffset >= 70) {
         header.classList.add("on");
         topbtn.classList.add("on");
     } else {
@@ -28,64 +42,36 @@ function scrollHeader() {
     }
 }
 
-function skillsScrollAnimate() {
-    window.addEventListener('scroll', function () {
-        const skills = document.querySelectorAll("circle.skillbar");
-        if (window.scrollY >= 1500 && window.scrollY <= 2650) {
-            skills.forEach((e) => {
-                e.classList.add("circle_animation");
-            });
-        } else {
-            skills.forEach((e) => {
-                e.classList.remove("circle_animation");
-            });
-        };
-    });
-}
-
 function clickedToggleBar() {
-    toggleBar.addEventListener("click", ()=>{
-        navmenu.classList.toggle("show");
-        header.classList.toggle("active");
-    });
+    navmenu.classList.toggle("show");
+    header.classList.toggle("active");
 }
 
-function clickedImage() {
-
-    gallary.addEventListener("click", ({ target }) => {
-        if (target.classList.contains("gallary-item")) {
-            $(modal).css({ "display": "flex", "align-items": "center", "flex-wrap": "wrap"})
+function clickedImage(target) {
+    if (target.classList.contains("gallary-item")) {
+        $(modal).css({ "display": "flex", "align-items": "center", "flex-wrap": "wrap" })
             .hide().fadeIn();
-            header.classList.remove("on");
-            topbtn.classList.remove("on");
-            closeBtn.classList.add("on");
-            body.style.overflow = "hidden";
-            modalImg.src = target.children[0].src;
-            modalImg.alt = target.children[0].alt;
-        }
-    });
-    zoomInImage();
-    closedImage();
-
-}
-
-function zoomInImage() {
-
-    modalImg.addEventListener("click", ({ target }) => {
-        target.classList.toggle("zoom");
-    });
-
-}
-
-function closedImage() {
-
-    closeBtn.addEventListener("click", () => {
-        closeBtn.classList.remove("on");
-        modalImg.classList.remove("zoom");
-        body.style.overflow = "overlay";
-        header.classList.add("on");
-        topbtn.classList.add("on");
+        [header, topbtn, closeBtn].forEach((el, i) => {
+            removeOrAdd((i === 2 ? "add" : "remove"), el, "on");
+        });
+        body.style.overflowX = "hidden";
+        modalImg.src = target.children[0].src;
+        modalImg.alt = target.children[0].alt;
+    } else if (target.className === "close on") {
+        [header, topbtn].forEach(el => removeOrAdd("add", el, "on"));
+        [target, modalImg].forEach((el, i) => removeOrAdd("remove", el, (i === 0 ? "on" : "zoom")));
+        body.style.overflowX = "overlay";
         $(modal).fadeOut();
-    });
+    } else if (target.className === "gallary-img" || "gallary-img zoom") {
+        target.classList.toggle("zoom");
+    }
 
+}
+
+function removeOrAdd(type, target, classname) {
+    if (type === "remove") {
+        target.classList.remove(classname);
+    } else {
+        target.classList.add(classname);
+    }
 }
